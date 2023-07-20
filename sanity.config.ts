a924945dddb3941575052255773544aad7444122
ericlam1114 <83894250+ericlam1114@users.noreply.github.com>
2023-07-20 10:47:17 -0700

    blog update

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,9 +6,12 @@ import {deskTool} from 'sanity/desk'
 import schema from './oliviaweld/sanity/schema'
 
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "uzas62mq";
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2021-10-21";
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+// const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "uzas62mq";
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+// const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
+// const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2021-10-21";
 
 // check for undefined
 if (!projectId || !dataset || !apiVersion) {
