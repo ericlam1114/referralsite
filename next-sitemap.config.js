62f9f6321bfa4d11dc49506bf69477e12e6282f9
ericlam1114 <83894250+ericlam1114@users.noreply.github.com>
2024-01-30 17:15:03 -0800

    sitemap build

---
+++ next-sitemap.config.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+let blogSlugs = [];
+try {
+  blogSlugs = JSON.parse(fs.readFileSync('blogUrls.json', 'utf8'));
+} catch (error) {
+  console.error('Error reading blog URLs file:', error);
+}
+
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://energycredittransfer.com',
+  generateRobotsTxt: true,
+  additionalPaths: async () => blogSlugs.map(slug => ({ loc: slug })),
+};
