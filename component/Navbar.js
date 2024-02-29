5532b6e0afd902ac869363ac2b5d858ecda512bf
ericlam1114 <83894250+ericlam1114@users.noreply.github.com>
2024-02-28 19:49:31 -0800

    push updates

--- component/Navbar.js
+++ component/Navbar.js
@@ -18,7 +18,7 @@ export default function Example() {
         <div className="flex lg:flex-1">
           <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Energy Credit Transfter</span>
-            <img className="h-20 w-auto" src="/startreferral.png" alt="logo" />
+            <img className="h-12 md:lg:h-20 w-auto" src="/startreferral.png" alt="logo" />
           </a>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
@@ -29,17 +29,17 @@ export default function Example() {
           ))}
         </div>
         <div className="flex flex-1 items-center justify-end gap-x-6">
-          <a href="/login" className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900">
+          <a href="/login" className=" block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900">
             Log in
           </a>
           <a
             href="/signup"
-            className="rounded-md bg-red-600 px-3  py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="rounded-md bg-red-600 px-3 py-2 text-sm md:lg:text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Sign up
           </a>
         </div>
-        <div className="flex lg:hidden">
+        {/* <div className="flex lg:hidden">
           <button
             type="button"
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
@@ -48,7 +48,7 @@ export default function Example() {
             <span className="sr-only">Open main menu</span>
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
-        </div>
+        </div> */}
       </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
@@ -58,7 +58,7 @@ export default function Example() {
               <span className="sr-only">Your Company</span>
               <img
                 className="h-8 w-auto"
-                src="/ectlogo.png"
+                src="/startreferral.png"
                 alt=""
               />
             </a>
